f721ab77475937bec29294d8ad3e6c3535f3e62d
CODING ENTHUSIAST <163559580+890y@users.noreply.github.com>
2024-09-29 12:01:55 +0500

    Delete Task 4/java-rmi-project/src directory

--- Task 4/java-rmi-project/src/ResourceSharing.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-import java.util.List;
-
-public interface ResourceSharing extends Remote {
-    void registerClient(String clientName) throws RemoteException;
-    String shareMessage(String message) throws RemoteException;
-    List<String> listResources() throws RemoteException;
-}
